abe4209cc3dd0055ec0545df2e4a8884677fabe0
chenyirou <annie851216@gmail.com>
2018-03-27 11:23:02 +0800

    轉換型別並轉換button文字

--- app/src/main/java/com/example/admin/ch5_4_3/MainActivity.java
+++ app/src/main/java/com/example/admin/ch5_4_3/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.admin.ch5_4_3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +16,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void button1_click(View view){
-        Toast.makeText(this, "Hello button1", Toast.LENGTH_LONG).show();
+        String button1Text = ((Button)view).getText().toString(); //找到button1的buttonText並轉換成文字
+        Toast.makeText(this, button1Text, Toast.LENGTH_LONG).show(); //顯示button1的buttonText提示文字
+        ((Button)view).setText("change button Text"); //將button1的buttonText文字替換掉
+
+
+        Button button2 = (Button)findViewById(R.id.button2); //利用button的id找到button2
+        button2.setText("i love sun"); //將button2的buttonText文字替換掉
     }
 
 }
